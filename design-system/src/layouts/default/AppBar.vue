26c3f17d9db559001a0f57972b45c79b8061710b
Fernando Vasconcelos <Fernando.Vasconcelos@anm.gov.br>
2023-09-19 17:23:50 -0300

    Criação da Barra da Aplicação

--- design-system/src/layouts/default/AppBar.vue
+++ design-system/src/layouts/default/AppBar.vue
@@ -1,12 +1,21 @@
 <template>
   <v-app-bar flat>
     <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-4" />
-
-      Base Preset
+      <v-icon icon="mdi-bookshelf" />
+      Biblioteca de Componentes do Tribunal de Contas do Distrito Federal
+      <v-icon icon="mdi-scale-balance" />
     </v-app-bar-title>
+    <div class="actions">
+      <v-icon icon="mdi-bell-ring-outline" />
+      <v-icon icon="mdi-cog-outline" />
+      <v-icon icon="mdi-translate" />
+    </div>
   </v-app-bar>
 </template>
+
+<style scoped>
+  .actions i { margin-right: 3mm; }
+</style>
 
 <script lang="ts" setup>
   //
